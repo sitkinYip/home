8f66fcca9994d80cab456194156fc53cc9b79636
叶诗鑫 <ysx01242962@alibaba-inc.com>
2026-01-22 09:25:41 +0800

    feat: 更新默认背景图链接。

--- src/assets/imgMap.js
+++ src/assets/imgMap.js
@@ -1,17 +1,30 @@
 const defaultImagesMap = {
-  background1: "https://blog.sitkin.top/upload/background1.jpg",
-  background2: "https://blog.sitkin.top/upload/background2.jpg",
-  background3: "https://blog.sitkin.top/upload/background3.jpg",
-  background4: "https://blog.sitkin.top/upload/background4.jpg",
-  background5: "https://blog.sitkin.top/upload/background5.png",
-  background6: "https://blog.sitkin.top/upload/background6.jpg",
-  background7: "https://blog.sitkin.top/upload/background7.jpg",
-  background8: "https://blog.sitkin.top/upload/background8.jpg",
-  background9: "https://blog.sitkin.top/upload/background9.jpg",
-  background10: "https://blog.sitkin.top/upload/background10.jpg",
-  background11: "https://blog.sitkin.top/upload/background11%20.jpeg",
-  background12: "https://blog.sitkin.top/upload/background12.jpg",
-  background13: "https://blog.sitkin.top/upload/photo1.jpeg",
+  background1:
+    "https://sitkin-cdn.oss-cn-heyuan.aliyuncs.com/pice/wallpaper/IMG_58491.jpg?sign=zYxZ59yVujHA5N8CeNBYtHNAJA_8NSdAqw0snpbc9zg=:0",
+  background2:
+    "https://sitkin-cdn.oss-cn-heyuan.aliyuncs.com/pice/wallpaper/IMG_58490.jpg?sign=vns6yruh8P8ZIexaS3Z5s2zJ0yON1uN8L8C1zpZteTk=:0",
+  background3:
+    "https://sitkin-cdn.oss-cn-heyuan.aliyuncs.com/pice/wallpaper/IMG_58488.jpg?sign=i-QXZcIUTYWwJpwtku-5PbB40p6_EWKI13QMlZNEujo=:0",
+  background4:
+    "https://sitkin-cdn.oss-cn-heyuan.aliyuncs.com/pice/wallpaper/IMG_58489.jpg?sign=Rh8XnE5YcODVJc3lW-45jbDpe11jSyFyPJiSpRXLeVg=:0",
+  background5:
+    "https://sitkin-cdn.oss-cn-heyuan.aliyuncs.com/pice/wallpaper/IMG_58486.jpg?sign=ekfqaUQ_6lztkb6zSoWPJiXqzdufYqZHMTR30tHRiVc=:0",
+  background6:
+    "https://sitkin-cdn.oss-cn-heyuan.aliyuncs.com/pice/wallpaper/IMG_58487.jpg?sign=1jJyqQIxSThKeY8LMyJgW4AW_YuOVUZRAD2mxe5Vbig=:0",
+  background7:
+    "https://sitkin-cdn.oss-cn-heyuan.aliyuncs.com/pice/wallpaper/IMG_58485.jpg?sign=AhrwNLqp6OEKcGrD-DpC3JvOISb3ZgqpgoODcElWH8M=:0",
+  background8:
+    "https://sitkin-cdn.oss-cn-heyuan.aliyuncs.com/pice/wallpaper/IMG_58483.jpg?sign=tW2IxlsSM69E_7WaqJq2xxBKJ9RixEIuFZKiQBrZXBE=:0",
+  background9:
+    "https://sitkin-cdn.oss-cn-heyuan.aliyuncs.com/pice/wallpaper/IMG_58482.jpg?sign=TOXJ-3ZO19CNsMv3BbhFesFjVxLCNMRCNIEvIdpx_nc=:0",
+  background10:
+    "https://sitkin-cdn.oss-cn-heyuan.aliyuncs.com/pice/wallpaper/IMG_58480.jpg?sign=_Ad5wwMUzTvT1j_pcRoBwU22EKJHwTKSob8xjH8wAwA=:0",
+  background11:
+    "https://sitkin-cdn.oss-cn-heyuan.aliyuncs.com/pice/wallpaper/IMG_58481.jpg?sign=7hlmT67X2Ifg46eCeGR2W8j1IJIvtId1DCIwX4b2iFo=:0",
+  background12:
+    "https://sitkin-cdn.oss-cn-heyuan.aliyuncs.com/pice/wallpaper/IMG_58484.jpg?sign=jVqjOaReADJJrwefHL6XLxOtMcpHuVdNRAaTO08FFs4=:0",
+  background13:
+    "https://sitkin-cdn.oss-cn-heyuan.aliyuncs.com/pice/wallpaper/IMG_58492.jpg?sign=EBD3uSa3NFcKseXq8P2Ti6MOc0fvWvzvCCMvKIao9Uo=:0",
 };
 
 export { defaultImagesMap };
