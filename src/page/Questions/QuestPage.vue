e1c7c27b399170e731775765a11e0cb46e07408e
sitkinYip <ysxdyx@gmail.com>
2026-03-09 22:08:42 +0800

    refactor(QuestPage): 移除未使用的视频提示提取函数
    - 删除了 extractVideoTips 函数，该函数不再被使用
    - 清理了相关的注释文档
    - 保持了原有的 AutoPlay 功能逻辑不变

--- src/page/Questions/QuestPage.vue
+++ src/page/Questions/QuestPage.vue
@@ -317,14 +317,6 @@ const handleHeaderClick = () => {
 let pendingFinalAutoPlay: { autoPlayType: AutoPlayResult; qaInfo: LevelRecord } | null = null;
 
 /**
- * 提取视频线索中的 tips 文字
- */
-const extractVideoTips = (thread: ThreadItemList): string | undefined => {
-  const videoItem = thread.find((t: ThreadItem) => t.type === "video" && t.tips);
-  return videoItem?.tips;
-};
-
-/**
  * 执行延迟的 AutoPlay（VictoryAura 关闭后调用）
  * binGo 已在 onConfirmAnswer 中 emit 过，此处仅执行媒体展示
  */
